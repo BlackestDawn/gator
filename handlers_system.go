7aae5a4c6ac80f15f3ecdb5344143576ce2f8364
Alexander Stauch <blacke4dawn@gmail.com>
2024-11-25 10:24:51 +0100

    chapter 3, minor reorg

--- handlers_system.go
+++ handlers_system.go
@@ -2,11 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"time"
-
-	"github.com/BlackestDawn/gator/internal/database"
-	"github.com/google/uuid"
 )
 
 func registerHandlers(c *commands) {
@@ -21,70 +16,4 @@ func registerHandlers(c *commands) {
 
 func handlerReset(s *state, cmd command) error {
 	return s.db.ResetUsers(context.Background())
-}
-
-func handlerLogin(s *state, cmd command) error {
-	if len(cmd.params) == 0 {
-		return fmt.Errorf("usage: gator login <name>")
-	}
-
-	name := cmd.params[0]
-
-	_, err := s.db.GetUser(context.Background(), name)
-	if err != nil {
-		return fmt.Errorf("could not find user: %w", err)
-	}
-
-	err = s.conf.SetUser(name)
-	if err != nil {
-		return fmt.Errorf("could not set user: %w", err)
-	}
-	fmt.Println("Successfully set user to:", s.conf.CurrentUserName)
-	return nil
-}
-
-func handlerRegister(s *state, cmd command) error {
-	if len(cmd.params) == 0 {
-		return fmt.Errorf("usage: gator register <name>")
-	}
-
-	name := cmd.params[0]
-	cTime := time.Now()
-	user := database.CreateUserParams{
-		ID:        uuid.New(),
-		CreatedAt: cTime,
-		UpdatedAt: cTime,
-		Name:      name,
-	}
-
-	retUser, err := s.db.CreateUser(context.Background(), user)
-	if err != nil {
-		return fmt.Errorf("could not create user: %w", err)
-	}
-
-	err = s.conf.SetUser(retUser.Name)
-	if err != nil {
-		return fmt.Errorf("could not set user: %w", err)
-	}
-
-	fmt.Println("Successfully created user:")
-	fmt.Println(retUser.Name)
-	return nil
-}
-
-func handlerListUsers(s *state, cmd command) error {
-	names, err := s.db.GetUsers(context.Background())
-	if err != nil {
-		return fmt.Errorf("failed to fetch users: %w", err)
-	}
-
-	for _, name := range names {
-		if name != s.conf.CurrentUserName {
-			fmt.Println("*", name)
-		} else {
-			fmt.Println("*", name, "(current)")
-		}
-	}
-
-	return nil
 }
